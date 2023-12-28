f62a8ddfb6f32142a550a57afa1cafc4e6a2d428
Mawgaming <132090903+Mawgaming@users.noreply.github.com>
2023-12-27 22:51:04 -0800

    Update

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript to interact with the Flask backend
+// Example: Function to send a request to the authenticate route
+function authenticateUser() {
+    // Implement AJAX request to '/authenticate'
+}
+// Add more functions to interact with other routes
